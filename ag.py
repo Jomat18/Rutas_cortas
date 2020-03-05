6adc7df9467dadb4ca38e5d1551fff4cbf4954a7
jtorresli <manu.jose.torres@gmail.com>
2020-03-05 17:57:09 -0500

    Updating files

--- ag.py
+++ ag.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import copy
 from numpy import loadtxt
@@ -8,22 +11,6 @@ import matplotlib.pyplot as plt
 
 distancias = loadtxt('road_distance.txt')
 
-'''
-distancias=[[0,12,3,23,1,5,23,56,12,11,89,97,52],
-            [12,0,9,18,3,41,45,5,41,27,16,76,56],
-             [3,9,0,89,56,21,12,48,14,29,5,91,8],
-             [23,18,89,0,87,46,75,17,50,42,100,70,15],
-             [1,3,56,87,0,55,22,86,14,33,31,84,21],
-             [5,41,21,46,55,0,21,76,54,81,92,37,22],
-             [23,45,12,75,22,21,0,11,57,48,39,59,22],
-             [56,5,48,17,86,76,11,0,63,24,55,58,98],
-             [12,41,14,50,14,54,57,63,0,9,44,18,52],
-             [11,27,29,42,33,81,48,24,9,0,64,65,82],		
-	     [89,16,5,100,31,92,39,55,44,64,0,9,70],
-	     [97,76,91,70,84,37,59,58,18,65,9,0,50],
-             [52,56,8,15,21,22,22,98,52,82,70,50,0]]
-
-'''
 nodos=[]
 for i in range(226):  #226
 	nodos.append(i)
@@ -305,16 +292,18 @@ def algoritmo_g(size,iteraciones,inicio,final,n_escalada,n_hijos):
 		print pob[i],"Costo: ",fitness(pob[i])	 	
 	
 
-pc=0.5
-pm=0.3
-size=30
-iteraciones=30
-n_escalada=1
-inicio=100
-final=206
-n_hijos=1
-algoritmo_g(size,iteraciones,inicio,final,n_escalada,n_hijos)
-plt.ioff(); plt.show()
+
+if __name__ == '__main__':
+	pc=0.5
+	pm=0.3
+	size=30
+	iteraciones=30
+	n_escalada=1
+	inicio=100
+	final=206
+	n_hijos=1
+	algoritmo_g(size,iteraciones,inicio,final,n_escalada,n_hijos)
+	plt.ioff(); plt.show()
 
 
 
